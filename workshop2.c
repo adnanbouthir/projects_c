1d698b2ef7aec1a431706106a1bca4aad387b831
adnanbouthir <adnanbouthir@gmail.com>
2021-01-14 17:02:28 +0100

    finished exercise 2 of step 1

--- workshop2.c
+++ workshop2.c
@@ -1,22 +1,22 @@
 #include <stdio.h>
-int a;
-int b;
+int a; // declare variable a
+int b; // declare variable b
 int main (void)
 {
-printf("Donnez le premier nombre \n");
-scanf("%d",&a);
+printf("input number a \n"); //prompt the user to input a number
+scanf("%d",&a); //takes user input and store it into a
 
-while(b == 0)
+while(b == 0) // while loop to make sure second number is bigger than 0 otherwise prompt user again
 {
-printf("Donnez le second nombre (Superieur a 0) \n");
-scanf("%d",&b);
+printf("input number b (Superior than 0) \n"); //prompt the user to input a number
+scanf("%d",&b); //takes user input and store it into b
 }
 
-int som = a+ b;
-int sous = a-b;
-int mult = a*b;
-int div = a/b;
-int mod = a%b;
-printf("a+b = %d \n a-b = %d \n a*b = %d \n a/b = %d \n a modulo b = %d\n",som,sous,mult,div,mod);
+int som = a + b; // calculate +
+int sous = a - b; // calculate -
+int mult = a * b; // calculate *
+int div = a / b; // calculate /
+int mod = a % b; // calculate % 
+printf("a+b = %d \n a-b = %d \n a*b = %d \n a/b = %d \n a modulo b = %d\n",som,sous,mult,div,mod); //displays the calculated results.
 
 }
